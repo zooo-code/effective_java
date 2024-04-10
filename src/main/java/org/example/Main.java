ddd4f18ad9f06da9617b9fc5caa59442f371b35a
zoo-code <kyj20908@naver.com>
2024-04-10 18:32:31 +0900

    2회독 시작

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,17 @@
 package org.example;
 
-import java.util.Collections;
-import java.util.concurrent.Executors;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
+        List<Object> a = new ArrayList<>();
+        a.add("String");
+        a.add(1);
+        System.out.println("a.get(0) = " + a.get(0));
+        System.out.println("a.get(1) = " + a.get(1));
 
         System.out.println("Hello world!");
     }
